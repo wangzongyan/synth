1614517c7ec0bd48ebd00459c4170294e31a9162
wangzongyan <wangzongyanjack@gmail.com>
2020-04-08 16:08:43 -0700

    first commit

---
+++ synth.py
@@ -0,0 +1,12 @@
+from sklearn.base import BaseEstimator
+
+
+class Synth(BaseEstimator):
+    def __init__(self):
+        pass
+
+    def fit(self):
+        pass
+
+    def predict(self):
+        pass
